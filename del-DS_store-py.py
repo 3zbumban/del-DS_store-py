5f3402b293a71352223f97d50617acbae5f70ce0
3zbumban <alexander.semenov@stud.fh-rosenheim.de>
2019-01-10 04:03:36 +0100

    asd

--- del-DS_store-py.py
+++ del-DS_store-py.py
@@ -18,8 +18,9 @@ CWD = os.getcwd()
 to_delete = ".DS_Store"
 
 argumetnparser = argparse.ArgumentParser(description="Usage: delete-dsstore.py -p/--path <PATH> or:  delete-dsstore.py -c/--cwd/--current-dir \nExample: delete-dsstore.py -p /Users/angelito")
-argumetnparser.add_argument("-p", "--path", dest="target_path", type=str, required=False, help="the path you want to start from as a string \"C:\example\dir\...\...\"")
+argumetnparser.add_argument("-p", "--path", dest="target_path", type=str, required=False, help="the path you want to start from as a string \"C:\\example\\dir\\...\\...\"")
 argumetnparser.add_argument("-c", "--current-dir", "--cwd", dest="use_cwd", action="store_true", help="add this flag to use the scripts dir as the starting point")
+argumetnparser.add_argument("-v", "--verbose", dest="v", action="store_true", help="outputs every file that gets checked for debug purpose, slows down the script")
 
 args, unknowns = argumetnparser.parse_known_args()
 
@@ -49,7 +50,7 @@ def main():
 			if(os.path.isdir(args.target_path)):
 				path = args.target_path
 			else:
-				print("[e] given path is not a dir")
+				print("[e] given path is not a dir\n[i] did you use a backslash (\"\\\") to much?")
 				exit(-1)
 		else:
 			print("[i] exit no path given...")
@@ -67,7 +68,8 @@ def main():
 				for root, sub, files in os.walk(path):
 						
 					for file in files:
-
+						if(args.v):
+							print("[i] checking: {}".format(os.path.abspath(os.path.join(root, file))))
 						# 4. Checks if exists .DS_Store file
 						if file == ".DS_Store":
 
@@ -83,14 +85,16 @@ def main():
 							i += 1
 				# 7. print result
 				end = time.time()
-				print("[i] deleted: {} \n[i] files and {} \n[i] time elapsed: {}".format(i, size(acc_f_size), end - start))
+				print("\n\n\n[i] number of deleted files: {} \n[i] filesize: {} \n[i] time elapsed: {:4.4f}sec".format(i, size(acc_f_size), (end - start) % 60))
 			else:
 				exit(0)
 		else:
 			print("[i] you choose to abort the script...")
 			exit(0)
 	except KeyboardInterrupt:
+		end = time.time()
 		print("[i] KeyboardINterrupt, aborting")
+		print("\n\n\n[i] number of deleted files: {} \n[i] filesize: {} \n[i] time elapsed: {:4.4f}sec".format(i, size(acc_f_size), (end - start) % 60))
 
 if __name__ == "__main__":
 	main()
